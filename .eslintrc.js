7d46337322e5b75adffb6418bab74117377701e7
malikanur <malika.kholdorova@zoftify.com>
2024-07-31 16:31:07 +0500

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  parser: '@typescript-eslint/parser',
+  plugins: ['simple-import-sort'],
+  rules: {
+    semi: ['error', 'never'],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-shadow': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'react-native/no-inline-styles': 'off',
+    '@typescript-eslint/no-explicit-any': ['error'],
+    'react-hooks/exhaustive-deps': 'off',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import {Text} from 'react-native'
+
+const App = () => {
+  return <Text style={{flex: 1}}>Hi there</Text>
+}
+export default App
